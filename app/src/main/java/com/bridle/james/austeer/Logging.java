6603c81998b5af2acd98df52ecb346a6a1d7dd15
stml <james@shorttermmemoryloss.com>
2017-03-12 18:16:19 +0200

    Logging to file

--- app/src/main/java/com/bridle/james/austeer/Logging.java
+++ app/src/main/java/com/bridle/james/austeer/Logging.java
@@ -8,24 +8,41 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Logging extends AppCompatActivity {
 
     TextView timerTextView;
     long startTime = 0;
 
+    private String filename = "Austeer-"+System.currentTimeMillis()+".txt";
+    private String filepath = "Austeer";
+    File envpath;
+    File outputFile;
+    String outputString = "";
+    FileWriter fw;
+
     TextView speedTextView;
     TextView locationTextView;
     TextView steeringTextView;
+    TextView storageTextView;
 
-    String locString = "Getting location...";
-    String speedString = "Getting speed...";
-    String steerString = "Getting Steering Angle...";
+    String locString = "";
+    String speedString = "";
     String steerStringX;
     String steerStringY;
     String steerStringZ;
@@ -49,10 +66,19 @@ public class Logging extends AppCompatActivity {
             seconds = seconds % 60;
 
             timerTextView.setText(String.format("%d:%02d", minutes, seconds));
-            locationTextView.setText("Location: "+locString);
-            speedTextView.setText("Speed: "+speedString);
+            locationTextView.setText(locString);
+            speedTextView.setText(speedString);
 
-            steeringTextView.setText("Gyro: "+steerString);
+            steeringTextView.setText(steerStringY);
+
+            outputString = String.format("%d:%02d:%02d", minutes, seconds, millis%1000)+","+locString+","+speedString+","+steerStringY+"\n";
+
+            try {
+                fw.write(outputString);
+                storageTextView.setText("Writing to "+filename);
+            } catch (IOException e) {
+                storageTextView.setText(getString(R.string.storage_problem));
+            }
 
             timerHandler.postDelayed(this, 500);
         }
@@ -62,6 +88,27 @@ public class Logging extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging);
+
+        storageTextView = (TextView) findViewById(R.id.storageTextView);
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            storageTextView.setText(getString(R.string.storage_writable));
+            envpath = Environment.getExternalStoragePublicDirectory(filepath);
+            outputFile = new File(envpath, filename);
+            envpath.mkdirs();
+            try {
+                fw = new FileWriter(outputFile);
+            } catch (IOException e) {
+                storageTextView.setText(getString(R.string.storage_problem));
+            }
+        }
+        else {
+            storageTextView.setText(getString(R.string.storage_not_writable));
+        }
+
+        final DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.CEILING);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -78,8 +125,8 @@ public class Logging extends AppCompatActivity {
         li = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                locString = Double.toString(location.getLatitude())+','+Double.toString(location.getLongitude());
-                speedString = Float.toString(location.getSpeed());
+                locString = df.format(location.getLatitude())+','+df.format(location.getLongitude());
+                speedString = df.format(location.getSpeed());
             }
             public void onStatusChanged(String provider, int status, Bundle extras) {}
             public void onProviderEnabled(String provider) {}
@@ -89,7 +136,12 @@ public class Logging extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, li);
 
         sMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
-        gyro = sMgr.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        gyro = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // With Accelerometer, flat on a table is X-0, Y-0, Z-10
+        // Held flat upward in portrait mode, X-0, y-10, Z-0
+        // Held flat upward in landscape: X-10, Y-0, Z-0
+        // Steering mode fixed to steering wheel landscape: Just use Y-value
 
         sev = new SensorEventListener() {
             public void onAccuracyChanged(Sensor sensor, int accuracy) {}
@@ -97,10 +149,9 @@ public class Logging extends AppCompatActivity {
                 float axisX = event.values[0];
                 float axisY = event.values[1];
                 float axisZ = event.values[2];
-                steerStringX = Float.toString(axisX);
-                steerStringY = Float.toString(axisY);
-                steerStringZ = Float.toString(axisZ);
-                steerString = "X: "+steerStringX+" / Y: "+steerStringY+" / Z: "+steerStringZ;
+                steerStringX = Float.toString(Math.round(axisX*10)/10);
+                steerStringY = String.format("%.1f", axisY/10);
+                steerStringZ = Float.toString(Math.round(axisZ*10)/10);
             }
         };
 
@@ -111,7 +162,30 @@ public class Logging extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
-        timerHandler.removeCallbacks(timerRunnable);
         sMgr.unregisterListener(sev);
+        timerHandler.removeCallbacks(timerRunnable);
+        try {
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sMgr.registerListener(sev, gyro, SensorManager.SENSOR_DELAY_FASTEST);
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    public File getAlbumStorageDir(Context context, String albumName) {
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            storageTextView.setText(getString(R.string.storage_problem));
+        }
+        return file;
+    }
+
 }
