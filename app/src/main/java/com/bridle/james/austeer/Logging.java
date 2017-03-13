6eb93a45a1ac75aa267b493513afff1f994796cf
stml <james@shorttermmemoryloss.com>
2017-03-13 14:39:29 +0200

    Speed calculation

--- app/src/main/java/com/bridle/james/austeer/Logging.java
+++ app/src/main/java/com/bridle/james/austeer/Logging.java
@@ -1,6 +1,10 @@
 package com.bridle.james.austeer;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,6 +13,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -29,7 +34,9 @@ public class Logging extends AppCompatActivity {
     TextView timerTextView;
     long startTime = 0;
 
-    private String filename = "Austeer-"+System.currentTimeMillis()+".csv";
+    private static final String TAG = "AusteerLogging";
+
+    private String filename = "Austeer-" + System.currentTimeMillis() + ".csv";
     private String filepath = "Austeer";
     File envpath;
     File outputFile;
@@ -46,6 +53,11 @@ public class Logging extends AppCompatActivity {
     String steerStringX;
     String steerStringY;
     String steerStringZ;
+
+    Double lastLat;
+    Double lastLng;
+    Double lastAlt;
+    Long lastTime;
 
     LocationManager locationManager;
     LocationListener li;
@@ -71,11 +83,11 @@ public class Logging extends AppCompatActivity {
 
             steeringTextView.setText(steerStringY);
 
-            outputString = String.format("%d:%02d:%02d", minutes, seconds, millis%1000)+","+locString+","+speedString+","+steerStringY+"\n";
+            outputString = String.format("%d:%02d:%02d", minutes, seconds, millis % 1000) + "," + locString + "," + speedString + "," + steerStringY + "\n";
 
             try {
                 fw.write(outputString);
-                storageTextView.setText("Writing to "+filename);
+                storageTextView.setText("Writing to " + filename);
             } catch (IOException e) {
                 storageTextView.setText(getString(R.string.storage_problem));
             }
@@ -102,8 +114,7 @@ public class Logging extends AppCompatActivity {
             } catch (IOException e) {
                 storageTextView.setText(getString(R.string.storage_problem));
             }
-        }
-        else {
+        } else {
             storageTextView.setText(getString(R.string.storage_not_writable));
         }
 
@@ -125,17 +136,56 @@ public class Logging extends AppCompatActivity {
         li = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                locString = df.format(location.getLatitude())+','+df.format(location.getLongitude());
-                speedString = df.format(location.getSpeed());
+
+                Double newLat = location.getLatitude();
+                Double newLng = location.getLongitude();
+                Double newAlt = location.getAltitude();
+                Long newTime = location.getTime();
+
+                locString = df.format(newLat) + ", " + df.format(newLng) + ", " + df.format(newAlt) + "m";
+
+                Log.v(TAG, "IN ON LOCATION CHANGE, lat=" + df.format(newLat) + ", lon=" + df.format(newLng));
+
+                if (lastLat == null) {
+                    speedString = "0";
+                } else {
+                    Double dist = distance(lastLat, newLat, lastLng, newLng, lastAlt, newAlt);
+                    Double speed = dist / ((newTime - lastTime) / 1000);
+                    speedString = df.format(speed);
+                    lastLat = newLat;
+                    lastLng = newLng;
+                    lastAlt = newAlt;
+                    lastTime = newTime;
+                }
+
             }
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-            public void onProviderEnabled(String provider) {}
-            public void onProviderDisabled(String provider) {}
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
         };
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permissions Needed")
+                    .setMessage("You need to give Austeer permission to use your location.")
+                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, li);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, li);
 
-        sMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         gyro = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         // With Accelerometer, flat on a table is X-0, Y-0, Z-10
@@ -144,14 +194,16 @@ public class Logging extends AppCompatActivity {
         // Steering mode fixed to steering wheel landscape: Just use Y-value
 
         sev = new SensorEventListener() {
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+
             public void onSensorChanged(SensorEvent event) {
                 float axisX = event.values[0];
                 float axisY = event.values[1];
                 float axisZ = event.values[2];
-                steerStringX = Float.toString(Math.round(axisX*10)/10);
-                steerStringY = String.format("%.1f", axisY/10);
-                steerStringZ = Float.toString(Math.round(axisZ*10)/10);
+                steerStringX = Float.toString(Math.round(axisX * 10) / 10);
+                steerStringY = String.format("%.1f", axisY / 10);
+                steerStringZ = Float.toString(Math.round(axisZ * 10) / 10);
             }
         };
 
@@ -188,4 +240,34 @@ public class Logging extends AppCompatActivity {
         return file;
     }
 
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * el2 End altitude in meters
+     * @returns Distance in Meters
+     */
+    public Double distance(double lat1, double lat2, double lon1,
+                                  double lon2, double el1, double el2) {
+
+        final int R = 6371; // Radius of the earth
+
+        Double latDistance = Math.toRadians(lat2 - lat1);
+        Double lonDistance = Math.toRadians(lon2 - lon1);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
+    }
+
 }
+
